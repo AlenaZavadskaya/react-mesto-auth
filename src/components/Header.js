50c7ba8284b06558d9a9ca6eaf9c6d50eb900e3b
AlenaZavadskaya <helen.zavadskaya@yandex.ru>
2021-01-09 22:56:33 +0300

    add authorization

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,31 @@
 import React from "react";
+import { Route, Link, Switch } from "react-router-dom";
 import Logo from "../images/logo.svg";
 
-function Header() {
+function Header(props) {
   return (
     <header className="header">
-			<img className="header__logo" src={Logo} alt="логотип" />
-			<div className="header__enter">
-				{/* <p className="header__email">email@mail.com</p>
-				<p className="header__auth">Выйти</p> */}
-				<p className="header__auth">Войти</p>
-			</div>
+      <img className="header__logo" src={Logo} alt="логотип" />
+      <div className="header__enter">
+        <Switch>
+          <Route path="/sign-up">
+            <Link to="./sign-in" className="header__auth">
+              Войти
+            </Link>
+          </Route>
+          <Route path="/sign-in">
+            <Link to="./sign-up" className="header__auth">
+              Регистрация
+            </Link>
+          </Route>
+          <Route path="/">
+            <p className="header__email">{props.email}</p>
+            <p className="header__auth" onClick={props.onSignOut}>
+              Выйти
+            </p>
+          </Route>
+        </Switch>
+      </div>
     </header>
   );
 }
