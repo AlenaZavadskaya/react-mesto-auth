cf8dfe58572889101a0a754db33d6f7a774d5a08
AlenaZavadskaya <helen.zavadskaya@yandex.ru>
2021-01-09 22:54:04 +0300

    add registration

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,37 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import RegisterForm from "./RegisterForm";
 
 function Register(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleChangeEmail(e) {
+    setEmail(e.target.value);
+  }
+
+  function handleChangePassword(e) {
+    setPassword(e.target.value);
+  }
+
+  function handleSubmit(e) {
+		e.preventDefault();
+    props.onRegister(email, password);
+  }
+
   return (
-    <section
-      className="popup popup_opened"
-      id={props.name}
-    >
-      <div className="popup__container">
-        <button
-          className="popup__close"
-          type="button"
-          onClick={props.onClose}
-        />
-        <form
-          className="form"
-          id={props.id}
-          onSubmit={props.onSubmit}
-          noValidate
-        >
-          <div className="form__container">
-            <h2 className="form__heading">{props.title}</h2>
-            <fieldset className="form__input-container">
-              {props.children}
-              <div className="form__handlers">
-                <button className="submit__button" type="submit">
-                  {props.isLoading}
-                </button>
-              </div>
-            </fieldset>
-          </div>
-        </form>
-      </div>
-    </section>
+    <>
+      <RegisterForm
+        name="Register"
+        id="form-register"
+        title="Регистрация"
+        isLoading={props.isLoading ? "Регистрация..." : "Зарегистрироваться"}
+        isOpen={props.isOpen}
+        onSubmit={handleSubmit}
+        Link={
+          <Link to="/sign-in" className="form__request-auth">
+            Уже зарегистрированны? Войти
+          </Link>
+        }
+      >
+        <label className="form__field form__field-email">
+          <input
+            id="email-input"
+            className="form__item form__item_auth"
+            type="text"
+            placeholder="Email"
+            minLength="2"
+            maxLength="40"
+            value={email}
+            onChange={handleChangeEmail}
+            required
+          />
+          <span id="name-input-error" className="form__item-error" />
+        </label>
+        <label className="form__field form__field-password">
+          <input
+            id="password-input"
+            className="form__item form__item_auth"
+            type="text"
+            placeholder="Пароль"
+            minLength="2"
+            maxLength="200"
+            value={password}
+            onChange={handleChangePassword}
+            required
+          />
+          <span id="about-input-error" className="form__item-error" />
+        </label>
+      </RegisterForm>
+
+      {/* <p className="form__request-auth">Уже зарегистрированы? Войти</p> */}
+    </>
   );
 }
 
