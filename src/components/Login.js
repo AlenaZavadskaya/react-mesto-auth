fc0f320e29796c4a03483c9ac5c7e5ee12488031
AlenaZavadskaya <helen.zavadskaya@yandex.ru>
2021-01-09 22:54:24 +0300

    add login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,37 +1,77 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import RegisterForm from "./RegisterForm";
 
 function Login(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleChangeEmail(e) {
+    setEmail(e.target.value);
+  }
+
+  function handleChangePassword(e) {
+    setPassword(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!email || !password) {
+      return;
+    }
+    props.onLogin(email, password);
+  }
+
+  useEffect(() => {
+    if (props.loggedIn) {
+      setEmail("");
+      setPassword("");
+    }
+  }, [props.loggedIn]);
+
   return (
-    <section
-      className={`popup ${props.isOpen ? "popup_opened" : ""}`}
-      id={props.name}
-    >
-      <div className="popup__container">
-        <button
-          className="popup__close"
-          type="button"
-          onClick={props.onClose}
-        />
-        <form
-          className="form"
-          id={props.id}
-          onSubmit={props.onSubmit}
-          noValidate
-        >
-          <div className="form__container">
-            <h2 className="form__heading">{props.title}</h2>
-            <fieldset className="form__input-container">
-              {props.children}
-              <div className="form__handlers">
-                <button className="submit__button" type="submit">
-                  {props.isLoading}
-                </button>
-              </div>
-            </fieldset>
-          </div>
-        </form>
-      </div>
-    </section>
+    <>
+      <RegisterForm
+        name="Enter"
+        id="form-enter"
+        title="Вход"
+        isLoading={props.isLoading ? "Вход..." : "Войти"}
+        isOpen={props.isOpen}
+        onSubmit={handleSubmit}
+      >
+        <label className="form__field form__field-email">
+          <input
+            id="email-input"
+            className="form__item form__item_auth"
+            type="email"
+            placeholder="Email"
+            minLength="2"
+            maxLength="40"
+            value={email}
+            onChange={handleChangeEmail}
+            required
+          />
+          <span id="name-input-error" className="form__item-error">
+            {props.message}
+          </span>
+        </label>
+        <label className="form__field form__field-password">
+          <input
+            id="password-input"
+            className="form__item form__item_auth"
+            type="password"
+            placeholder="Пароль"
+            minLength="2"
+            maxLength="200"
+            value={password}
+            onChange={handleChangePassword}
+            required
+          />
+          <span id="about-input-error" className="form__item-error">
+            {props.message}
+          </span>
+        </label>
+      </RegisterForm>
+    </>
   );
 }
 
